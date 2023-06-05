bc94d0791942f6a94f6cd3878dd154ff9b52b707
igorcossta <igorcossta21@gmail.com>
2023-06-05 16:23:57 -0300

    refactor: replace all authorizeHttpRequests methods to only one

--- src/main/java/io/igorcossta/security/config/SecurityConfig.java
+++ src/main/java/io/igorcossta/security/config/SecurityConfig.java
@@ -1,12 +1,8 @@
 package io.igorcossta.security.config;
 
-import io.igorcossta.security.user.Permission;
-import io.igorcossta.security.user.Role;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static io.igorcossta.security.user.Permission.*;
-import static io.igorcossta.security.user.Role.*;
+import static io.igorcossta.security.user.Role.ADMIN;
+import static io.igorcossta.security.user.Role.USER;
 import static org.springframework.http.HttpMethod.*;
 
 @Configuration
@@ -31,34 +28,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(manager -> manager.requestMatchers("/api/v1/auth/**").permitAll())
-
-//                user endpoints
-                .authorizeHttpRequests(manager -> manager.requestMatchers("/api/v1/user/**")
-                        .hasAnyRole(ADMIN.name(), USER.name()))
-
-                .authorizeHttpRequests(manager -> manager.requestMatchers(GET, "/api/v1/user/**")
-                        .hasAnyAuthority(USER_READ.name(), ADMIN_READ.name()))
-
-                .authorizeHttpRequests(manager -> manager.requestMatchers(POST, "/api/v1/user/**")
-                        .hasAnyAuthority(USER_CREATE.name(), ADMIN_CREATE.name()))
-
-                .authorizeHttpRequests(manager -> manager.requestMatchers(DELETE, "/api/v1/user/**")
-                        .hasAnyAuthority(USER_DELETE.name(), ADMIN_DELETE.name()))
-
-//                admin endpoints
-                .authorizeHttpRequests(manager -> manager.requestMatchers("/api/v1/admin/**")
-                        .hasRole(ADMIN.name()))
-                .authorizeHttpRequests(manager -> manager.requestMatchers(GET, "/api/v1/admin/**")
-                        .hasAuthority(ADMIN_READ.name()))
-
-                .authorizeHttpRequests(manager -> manager.requestMatchers(POST, "/api/v1/admin/**")
-                        .hasAuthority(ADMIN_CREATE.name()))
-
-                .authorizeHttpRequests(manager -> manager.requestMatchers(DELETE, "/api/v1/admin/**")
-                        .hasAuthority(ADMIN_DELETE.name()))
-
-                .authorizeHttpRequests(manager -> manager.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        // user endpoints
+                        .requestMatchers("/api/v1/user/**").hasAnyRole(ADMIN.name(), USER.name())
+                        .requestMatchers(GET, "/api/v1/user/**").hasAnyAuthority(USER_READ.name(), ADMIN_READ.name())
+                        .requestMatchers(POST, "/api/v1/user/**").hasAnyAuthority(USER_CREATE.name(), ADMIN_CREATE.name())
+                        .requestMatchers(DELETE, "/api/v1/user/**").hasAnyAuthority(USER_DELETE.name(), ADMIN_DELETE.name())
+                        // admin endpoints
+                        .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+                        .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+                        .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
+                        .anyRequest().authenticated()
+                )
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
